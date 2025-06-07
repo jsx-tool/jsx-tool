c305e79e4d436d3e0c8628ab4587d902b323921d
Jamie Sunderland <jamesrainersunderland@gmail.com>
2025-06-07 14:20:45 -0700

    [jrs] added referrers to forwarded messages

--- src/services/desktop-emitter.service.ts
+++ src/services/desktop-emitter.service.ts
@@ -1,12 +1,14 @@
 import { injectable, singleton, inject } from 'tsyringe';
 import { FilemapDesktopSocketService } from './filemap-desktop-socket.service';
 import type { WebSocketInboundEvent } from './websocket.service';
+import { ConfigService } from './config.service';
 
 interface RegisterUuidEvent {
   event: 'register_uuid'
   data: {
     uuid: string
     expirationTime: string
+    referrer: string
   }
 }
 
@@ -14,19 +16,27 @@ type ForwardedMessage =
   WebSocketInboundEvent<'open_element'> |
   WebSocketInboundEvent<'open_file'>;
 
+type ForwardedMessageWithReferrer = ForwardedMessage & {
+  referrer: string
+};
+
 @singleton()
 @injectable()
 export class DesktopEmitterService {
   constructor (
+    @inject(ConfigService) private readonly configService: ConfigService,
     @inject(FilemapDesktopSocketService) private readonly socketService: FilemapDesktopSocketService
   ) {}
 
   public registerUuid (uuid: string, expirationTime: string): void {
+    const { wsHost, wsPort, wsProtocol } = this.configService.getConfig();
+    const referrer = `${wsProtocol}://${wsHost}:${wsPort}`;
     const event: RegisterUuidEvent = {
       event: 'register_uuid',
       data: {
         uuid,
-        expirationTime
+        expirationTime,
+        referrer
       }
     };
 
@@ -34,7 +44,18 @@ export class DesktopEmitterService {
     this.socketService.broadcast(message);
   }
 
+  private appendReferrerToForwardedMessage (message: ForwardedMessage): ForwardedMessageWithReferrer {
+    const { wsHost, wsPort, wsProtocol } = this.configService.getConfig();
+    const referrer = `${wsProtocol}://${wsHost}:${wsPort}`;
+    return {
+      ...message,
+      referrer
+    };
+  }
+
   public forwardMessage (message: ForwardedMessage) {
-    this.socketService.broadcast(JSON.stringify(message));
+    this.socketService.broadcast(
+      JSON.stringify(this.appendReferrerToForwardedMessage(message))
+    );
   }
 }
