10676b5847f73fbcdbbbf85d2e813a7b813d07e7
Dan Hyman <danhyman91@gmail.com>
2025-07-13 10:47:35 -0400

    tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 describe('CLI entry point', () => {
-  it('should load without errors', () => {
-    expect(() => require('./index')).not.toThrow();
+  it('should load without errors', async () => {
+    await expect(async () => await import('./index')).not.toThrow();
   });
 });
